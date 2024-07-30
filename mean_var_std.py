c6599b4dac1b8978474c5aa1dd8bc9374cc04142
khidd1 <123312065+khidd1@users.noreply.github.com>
2024-07-29 22:41:29 -0400

    mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,22 @@ import numpy as np
 
 def calculate(list):
 
-
+def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    # Convert list to 3x3 Numpy array
+    array = np.array(list).reshape(3, 3)
+    
+    # Calculate the required statistics
+    calculations = {
+        'mean': [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array).tolist()],
+        'variance': [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array).tolist()],
+        'standard deviation': [np.std(array, axis=0).tolist(), np.std(array, axis=1).tolist(), np.std(array).tolist()],
+        'max': [np.max(array, axis=0).tolist(), np.max(array, axis=1).tolist(), np.max(array).tolist()],
+        'min': [np.min(array, axis=0).tolist(), np.min(array, axis=1).tolist(), np.min(array).tolist()],
+        'sum': [np.sum(array, axis=0).tolist(), np.sum(array, axis=1).tolist(), np.sum(array).tolist()]
+    }
 
 
     return calculations
